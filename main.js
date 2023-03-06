200e2bc906eb50242ad2fb4bda0fb8a481595684
Talisson Barbosa <talissonbarbosa@gmail.com>
2023-03-06 17:41:49 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    console.log($('header button'))
+
+    $('header button').click(function(){
+        $('form').slideDown()
+    })
+
+    $('#btn-cancelar').click(function(){
+        $('form').slideUp()
+    
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault()
+        const enderecoDaImagemNova = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${enderecoDaImagemNova}" />`).appendTo(novoItem)
+        $(`
+            <div class="overlay-imagem-link">
+                <a href= "${enderecoDaImagemNova}" target="_blank" title = "Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem)
+
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000)
+
+        $('#endereco-imagem-nova').val('')
+
+        
+    })
+    })
+        
+    
